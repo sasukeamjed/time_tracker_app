0950a51000e4e4f179ea5421bdd7ce47a475894e
sasukeamjed <sasukeamjed@gmail.com>
2019-08-18 06:13:46 +0400

    Initial commit

---
+++ lib/app/sign_in/sing_in_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 2.0,
+        title: Text('Time Tracker'),
+      ),
+      body: Container(
+        color: Colors.yellow,
+        child: Column(
+          children: <Widget>[
+            Container(
+              color: Colors.orange,
+              
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker_app/app/sign_in/sing_in_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Time Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: SignInPage(),
+    );
+  }
+}
